ada5db4f07c38863377e3de8a8c3bb2bcd91d024
Edward Cordero <ed.cordero@gmail.com>
2025-04-02 10:50:08 -0400

    destroy Observable, use inject(), and remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { ProfileComponent } from './profile/profile.component';
 import { ExpertiseComponent } from './expertise/expertise.component';
 import { ExperienceComponent } from './experience/experience.component';
-import { PriorExperienceComponent } from './prior-experience/prior-experience.component';
 import { EducationComponent } from './education/education.component';
 import { ResumeDataService } from './resume-data.service';
 import { ContactComponent } from './contact/contact.component';
 import { IExperience, IResume } from '../resume.data.model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -24,15 +24,19 @@ import { IExperience, IResume } from '../resume.data.model';
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  title = 'resume-app';
+  title = 'Edward Cordero - Resume';
   resumeData: IResume | undefined;
   currentExperience: IExperience[] = [];
   previousExperience: IExperience[] = [];
+  elementRef = inject(ElementRef);
+  resumeDataService = inject(ResumeDataService);
+  resumeData$: Observable<IResume[]>;
 
-  constructor(
-    private elementRef: ElementRef,
-    private resumeDataService: ResumeDataService,
-  ) {}
+  constructor() {
+    this.resumeData$ = this.resumeDataService
+      .getData()
+      .pipe(takeUntilDestroyed());
+  }
 
   ngOnInit() {
     console.log('ngOnInit');
@@ -42,17 +46,17 @@ export class AppComponent implements OnInit {
     this.getResumeData();
   }
   getResumeData() {
-    this.resumeDataService.getData().subscribe((data: IResume[]) => {
-      this.resumeData = data[0];
-      this.currentExperience = this.resumeData.experience.filter(
-        (exp) => exp.isRecent === true,
-      );
-      this.previousExperience = this.resumeData.experience.filter(
-        (exp) => exp.isRecent !== true,
-      );
-      console.log('this.resumeData', this.resumeData);
-      console.log('this.resumeData.name', this.resumeData.name);
-      console.log('this.resumeData.location', this.resumeData.location);
+    this.resumeData$.subscribe({
+      next: (data) => {
+        this.resumeData = data[0];
+        this.currentExperience = this.resumeData.experience.filter(
+          (exp) => exp.isRecent === true,
+        );
+        this.previousExperience = this.resumeData.experience.filter(
+          (exp) => exp.isRecent !== true,
+        );
+      },
+      error: (error) => console.log(error),
     });
   }
 }
